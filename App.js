8202b325ea75f84f22a3d9423cfb0a487abcd7e5
NeerajMac <neerajmac@NeerajMacs-Mac-mini.local>
2018-06-22 14:58:01 +0530

    yellow color added fo bg

--- App.js
+++ App.js
@@ -56,7 +56,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'green',
+    backgroundColor: 'yellow',
   },
   welcome: {
     fontSize: 20,
