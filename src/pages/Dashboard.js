5de0fcc552a2d66374a65338b0ca7fa0347b6583
Neeraj Prajapati <NeerajPrajapati@Admins-MacBook-Pro.local>
2018-06-27 19:45:25 +0530

    Android setup on macbook

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -141,7 +141,7 @@ export default class Pin extends Component {
         </View>
         <View style={{backgroundColor:'white',height:40,alignSelf:'stretch', justifyContent:'flex-end'}}>
             <TouchableOpacity onPress={this.onButtonPress}>
-                <Text style = {{textAlign:'center',textAlignVertical:'center',backgroundColor:'white',height:40,paddingTop:10, fontSize:15,fontWeight:'800'}}>Check Update</Text>
+                <Text style = {{textAlign:'center',textAlignVertical:'center',backgroundColor:'green',height:40,paddingTop:10, fontSize:15,fontWeight:'800'}}>Check Update</Text>
             </TouchableOpacity>
         </View>
       </View>
